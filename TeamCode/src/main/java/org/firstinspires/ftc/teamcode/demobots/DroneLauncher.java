95a7873b2b2ce2dafde4a8bdf6615774a4fb13b3
GearFreaks <ftc4991@gmail.com>
2025-05-13 19:15:39 -0400

    Updated DroneLauncher Added: - speed variable for chassis and buttons to control them   - "dpad-up" set speed to full   - "dpad-down" sets speed to half   - "dpad-right" sets speed to 0.75 Changes: - default chassis speed is 0.75

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demobots/DroneLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demobots/DroneLauncher.java
@@ -8,13 +8,28 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="DroneLauncher")
 public class DroneLauncher extends LinearOpMode {
     DcMotor Drone_Motor  = null;
+    DcMotor leftWheel = null;
+    DcMotor rightWheel = null;
     CRServo Drone_Servo  = null;
+    double leftY;
+    double rightY;
+    double speed = 0.75;
 
     public void runOpMode() {
         Drone_Motor = hardwareMap.get(DcMotor.class, "Drone_Motor");
+        leftWheel = hardwareMap.get(DcMotor.class, "left");
+        rightWheel = hardwareMap.get(DcMotor.class, "right");
         Drone_Servo = hardwareMap.get(CRServo.class, "Drone_Servo");
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
         while(opModeIsActive()) {
+
+           leftY  = gamepad1.left_stick_y;
+           rightY  = gamepad1.right_stick_y;
+
+            leftWheel.setPower(leftY * speed);
+            rightWheel.setPower(rightY * speed);
+
             if (gamepad1.a) {
                 Drone_Motor.setPower(1);
                 sleep(500);
@@ -23,6 +38,13 @@ public class DroneLauncher extends LinearOpMode {
                 sleep(300);
                 Drone_Motor.setPower(0);
                 Drone_Servo.setPower(0);
+            }
+            if (gamepad1.dpad_up) {
+                speed = 1;
+            } else if (gamepad1.dpad_down) {
+                speed = 0.5;
+            } else if (gamepad1.dpad_right) {
+                speed = 0.75;
             }
         }
     }
